9b9530300b9935d05565a960aa8012e64a71bd16
bamjam101 <herejamil123@gmail.com>
2022-08-07 12:47:00 +0530

    Connected to MongoDB using ODM library Mongoose. And created a database on MongoDB Atlas.

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 // create Express app
 const app = express();
 
+// connect to MongoDB
+const dbURI = "mongodb+srv://bamjam101:randomAccess@quimzz.r3fab.mongodb.net/bloggie-db?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+// declare public scope using express static
 app.use(express.static('public'));
+
 // use view engine
 app.set('view engine', 'ejs')
 
-app.listen(3000);
-
 // Routes
 app.get('/', (req,res)=> {
-    const blogs = [];
-    res.render('index', {title: 'Home', blogs: blogs});
+    Blog.find().sort({ createdAt: -1})
+        .then((result) => {
+            res.render('index', {title: 'Home', blogs: result});
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 app.get('/about', (req,res)=> {
@@ -25,8 +38,8 @@ app.get('/contact', (req,res)=> {
 app.get('/create', (req,res)=> {
     res.render('create', {title: 'Create New Blog'});
 });
-// Return Error page for invalid path
 
+// Return Error page for invalid path
 app.use((req, res) => {
     res.render('404', {title: 'Error'})
 })
